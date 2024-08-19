7c570ea8108c8f863aa99631a90663356a43045b
sumin <mins19960091@gmail.com>
2024-08-19 18:04:31 +0900

    스프링 api 사용해서 데이터 값을 전달해줄때 화면에 키 벨류값 띄우기

--- src/main/java/com/example/springpractice/controller/HelloController.java
+++ src/main/java/com/example/springpractice/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.springpractice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,41 @@ public class HelloController {
     public String hello(Model model){
         model.addAttribute("data","hello!!");
         return "hello";
+    }
+
+    //외부에서 파라미터 받기
+    @GetMapping("hello-MVC")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //html 바디에 직접 데이터를 넣어줄께
+    @GetMapping("hello-controller")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    //만약에 데이터를 내놓으라고 하는 상황일땐?
+    @GetMapping("hello-api")
+    @ResponseBody
+
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName(){
+            return name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
     }
 }
